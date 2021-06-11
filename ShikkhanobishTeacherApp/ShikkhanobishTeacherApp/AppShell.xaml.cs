d407ec6c36ef5e1fc29340854f26714e3e778d63
Mahir <mahirmuzahid@gmail.com>
2021-06-11 22:51:32 +0600

    Add project files.

---
+++ ShikkhanobishTeacherApp/ShikkhanobishTeacherApp/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using ShikkhanobishTeacherApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ShikkhanobishTeacherApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+            Routing.RegisterRoute(nameof(Profile), typeof(Profile));
+            Shell.SetNavBarIsVisible(this, false);
+        }
+
+    }
+}
