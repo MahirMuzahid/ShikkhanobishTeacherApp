631d04cd2979879c41e03dca02810ccbdf1a8b38
Mahir Muzahid <mahirmuzahid@gmail.com>
2021-07-23 12:34:42 +0600

    vc

---
+++ ShikkhanobishTeacherApp/ShikkhanobishTeacherApp/View Model/VideoCallPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ShikkhanobishTeacherApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ShikkhanobishTeacherApp.View_Model
+{
+    public class VideoCallPageViewModel: BaseViewMode, INotifyPropertyChanged
+    {
+
+        #region Methods
+        #endregion
+
+
+        #region Bindings
+        private string toalEarned1;
+
+        public string toalEarned { get => toalEarned1; set => SetProperty(ref toalEarned1, value); }
+
+        private string time1;
+
+        public string time { get => time1; set => SetProperty(ref time1, value); }
+        #endregion
+    }
+}
