ca9d2b7464d5aa057a8ec1de792d1ba9b4ea3b60
Mahir Muzahid <mahirmuzahid@gmail.com>
2021-08-20 00:09:31 +0600

    Teacher UI done

---
+++ ShikkhanobishTeacherApp/ShikkhanobishTeacherApp.Android/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShikkhanobishTeacherApp.Droid
+{
+    [Activity(Label = "ShikkhanobishTeacherApp", Icon = "@mipmap/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
