30ede0388d1c527b359bc4cb4ae7744da7d924e5
Mahir Muzahid <mahirmuzahid@gmail.com>
2021-08-23 20:28:46 +0600

    QUiz

--- ShikkhanobishTeacherApp/ShikkhanobishTeacherApp/Views/QuestionPage.xaml.cs
+++ ShikkhanobishTeacherApp/ShikkhanobishTeacherApp/Views/QuestionPage.xaml.cs
@@ -13,9 +13,14 @@ namespace ShikkhanobishTeacherApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QuestionPage : ContentPage
     {
+        Color SelectedColor = new Color();
+        Color notSelectedColor = new Color();
+        
         public QuestionPage()
         {
             InitializeComponent();
+            SelectedColor = Color.FromHex("#E1CFFF");
+            notSelectedColor = Color.Transparent;
         }
         protected override bool OnBackButtonPressed()
         {
@@ -31,6 +36,39 @@ namespace ShikkhanobishTeacherApp.Views
             {
                 Application.Current.MainPage.Navigation.PopModalAsync();          
             }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            opone.BackgroundColor = SelectedColor;
+            optwo.BackgroundColor = notSelectedColor;
+            opthree.BackgroundColor = notSelectedColor;
+            opfour.BackgroundColor = notSelectedColor;
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            opone.BackgroundColor = notSelectedColor;
+            optwo.BackgroundColor = SelectedColor;
+            opthree.BackgroundColor = notSelectedColor;
+            opfour.BackgroundColor = notSelectedColor;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            opone.BackgroundColor = notSelectedColor;
+            optwo.BackgroundColor = notSelectedColor;
+            opthree.BackgroundColor = SelectedColor;
+            opfour.BackgroundColor = notSelectedColor;
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            opone.BackgroundColor = notSelectedColor;
+            optwo.BackgroundColor = notSelectedColor;
+            opthree.BackgroundColor = notSelectedColor;
+            opfour.BackgroundColor = SelectedColor;
         }
     }
 }
