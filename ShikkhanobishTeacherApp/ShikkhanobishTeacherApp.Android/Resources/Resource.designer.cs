7d0a690f544353c3588d52aa5d58b694318075ef
Mahir Muzahid <mahirmuzahid@gmail.com>
2021-09-12 15:53:46 +0600

    Last Pre Update

--- ShikkhanobishTeacherApp/ShikkhanobishTeacherApp.Android/Resources/Resource.designer.cs
+++ ShikkhanobishTeacherApp/ShikkhanobishTeacherApp.Android/Resources/Resource.designer.cs
@@ -23598,76 +23598,79 @@ namespace ShikkhanobishTeacherApp.Droid
 			public const int rating = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int report = 2131165353;
+			public const int regback = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int selectionstatus = 2131165354;
+			public const int report = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int shikcon = 2131165355;
+			public const int selectionstatus = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int splash = 2131165356;
+			public const int shikcon = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int splashscreenlogo = 2131165357;
+			public const int splash = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int subject = 2131165358;
+			public const int splashscreenlogo = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int teacher = 2131165359;
+			public const int subject = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int teacherregback = 2131165360;
+			public const int teacher = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int test_custom_background = 2131165361;
+			public const int teacherregback = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int tooltip_frame_dark = 2131165362;
+			public const int test_custom_background = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int tooltip_frame_light = 2131165363;
+			public const int tooltip_frame_dark = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int totalmin = 2131165364;
+			public const int tooltip_frame_light = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int tuition = 2131165365;
+			public const int totalmin = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int withdraw = 2131165366;
+			public const int tuition = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int xamarin_logo = 2131165367;
+			public const int withdraw = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int xf_arrow_dropdown = 2131165368;
+			public const int xamarin_logo = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int xf_arrow_right = 2131165369;
+			public const int xf_arrow_dropdown = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int xf_check = 2131165370;
+			public const int xf_arrow_right = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int xf_checkbox_selected = 2131165371;
+			public const int xf_check = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int xf_checkbox_unselected = 2131165372;
+			public const int xf_checkbox_selected = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int xf_clear = 2131165373;
+			public const int xf_checkbox_unselected = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int xf_error = 2131165374;
+			public const int xf_clear = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int xf_radio_button_selected = 2131165375;
+			public const int xf_error = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int xf_radio_button_unselected = 2131165376;
+			public const int xf_radio_button_selected = 2131165376;
+			
+			// aapt resource value: 0x7F0700C1
+			public const int xf_radio_button_unselected = 2131165377;
 			
 			static Drawable()
 			{
